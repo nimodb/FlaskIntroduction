f89c67d78a12dce5e4b733b2b454370ae2271f5e
Nima Saadati <nimodbdev@gmail.com>
2024-07-03 13:07:40 +0400

    Initialized the database and created the Todo table

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Task {self.id}>"
 
 
 @app.route("/")
@@ -10,4 +24,6 @@ def index():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
